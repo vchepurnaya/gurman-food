b5cb29d3f03d0d3d6c3c733f36ccb03e3f927a73
useyourillusions <minsk.bay@gmail.com>
2020-07-09 23:33:01 +0300

    Added user container component

---
+++ src/app/modules/user/containers/user-container/user-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-container',
+  templateUrl: './user-container.component.html',
+  styleUrls: ['./user-container.component.scss']
+})
+export class UserContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
