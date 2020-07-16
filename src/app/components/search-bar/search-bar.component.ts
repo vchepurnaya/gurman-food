60c728008004dcff4cd9e4c09884a88301828424
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2020-07-16 19:52:45 +0300

    Edited search bar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  searchtext: string = 'paris';
+  searchtext: string = '';
 
   constructor() { }
 
