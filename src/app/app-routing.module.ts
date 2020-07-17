f3a4594374e9b932a00655a8a912df6c2dbea865
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2020-07-18 02:01:42 +0300

    added rest component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
+import { RestaurantsComponent } from './components/restaurants/restaurants.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: HomeComponent
+  },
+  {
+    path: 'restaurants',
+    component: RestaurantsComponent
   },
   {
     path: 'user',
