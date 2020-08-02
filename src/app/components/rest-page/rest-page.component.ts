972b7a52eab3c52773e03b3353fdad31aa49cea1
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2020-08-02 20:29:59 +0300

    Added reviews

--- src/app/components/rest-page/rest-page.component.ts
+++ src/app/components/rest-page/rest-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '@app/services';
 import { ActivatedRoute} from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { RestaurantsDefinition } from '@app/shared/interfaces'
+
 
 @Component({
   selector: 'app-rest-page',
@@ -8,16 +11,27 @@ import { ActivatedRoute} from '@angular/router';
   styleUrls: ['./rest-page.component.scss']
 })
 export class RestPageComponent implements OnInit {
+  restaurant: RestaurantsDefinition = null;
   id: string;
+  iframeSrc: SafeResourceUrl = null;
 
   constructor(
     private apiService: ApiService,
     private activatedRoute: ActivatedRoute,
-  ) {
+    private domSanitizer: DomSanitizer,
+  ) { 
     this.id = this.activatedRoute.snapshot.params['id'];
   }
 
   ngOnInit(): void {
-    console.log(this.id)
+
+    this.apiService.getRestaurantById({id:this.id})
+      .subscribe(
+        (success: { content: RestaurantsDefinition }) => {
+          this.restaurant = success.content;
+          this.iframeSrc = this.domSanitizer.bypassSecurityTrustResourceUrl('https://yandex.ru/maps-reviews-widget/' + success.content.mapId + '?comments');
+        },
+        error => console.log(error)
+      )
   }
 }
